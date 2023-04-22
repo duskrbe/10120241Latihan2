1e73421b4ed09f1fa89a9b3a317e624d0bea01c3
Arby Febrian Saputro <oginkacida2@gmail.com>
2023-04-23 00:35:51 +0700

    Penambahan tampilan (Register page view)

---
+++ app/src/main/java/edu/arbyfebrian/a10120241latihan2/MainActivity2.kt
@@ -0,0 +1,11 @@
+package edu.arbyfebrian.a10120241latihan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
